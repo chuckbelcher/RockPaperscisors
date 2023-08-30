fc54dbc72d1099d2677f6f144d8024d77e7645f0
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-30 19:24:27 -0400

    Initial Commit

---
+++ RockPaperscisors/RockPaperscisorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperscisorsApp.swift
+//  RockPaperscisors
+//
+//  Created by Chuck Belcher on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperscisorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
