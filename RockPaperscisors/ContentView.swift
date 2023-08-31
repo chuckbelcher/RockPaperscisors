d5fa427344000ab0ea2036540bb969a5939c35c2
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-31 10:21:30 -0400

    Added function to implement the logic to play a round, end the game, and reset once the game is over.

--- RockPaperscisors/ContentView.swift
+++ RockPaperscisors/ContentView.swift
@@ -11,8 +11,10 @@ struct ContentView: View {
     var moves = ["👊", "✋", "✌️"]
     @State private var computerChoice = Int.random(in: 0..<3)
     @State private var score = 0
-    
     @State private var shouldWin = Bool.random()
+    @State private var questionNumber = 0
+    @State private var showingGameOverAlert = false
+    
     var body: some View {
         VStack {
             Text("Rock Paper Scissors")
@@ -38,7 +40,7 @@ struct ContentView: View {
             HStack {
                 ForEach((0...2), id: \.self) { option in
                     Button {
-                        print("selected option \(option)")
+                        playRound(choice: option)
                     } label: {
                         Text(moves[option])
                             .font(.system(size: 100))
@@ -52,6 +54,53 @@ struct ContentView: View {
                 .padding(.top, 30)
         }
         .padding()
+        .alert("Reset Game", isPresented: $showingGameOverAlert) {
+            Button {
+                reset()
+            } label: {
+                Text("Reset Game")
+            }
+
+        } message: {
+            Text("Your final score is \(score)")
+        }
+    }
+    
+    func reset() {
+        print("Resetting Game")
+        questionNumber = 0
+        score = 0
+        computerChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+    }
+    
+    func playRound(choice: Int) {
+        let winningOptions = [1, 2, 0]
+        let loosingOptions = [2, 0, 1]
+        var didWin = false
+        var didLoose = false
+        if choice  == winningOptions[computerChoice] {
+            didWin = true
+        } else if choice == loosingOptions[computerChoice] {
+            didLoose = true
+        }
+        
+        //Check to see if we should have won
+        if shouldWin == true && didWin == true {
+            score += 1
+        } else if shouldWin == false && didLoose == true {
+            score += 1
+        } else {
+            score -= 1
+        }
+        computerChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+        questionNumber += 1
+        
+        if questionNumber == 10 {
+            print("Game Over")
+            showingGameOverAlert = true
+        }
     }
 }
 
